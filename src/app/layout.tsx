898f53797ea69df1562b00ca444d913436b03823
Heine Günther <heine_gunther@hotmail.com>
2024-04-24 15:38:07 +0200

    deleted a component file and added new favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import {
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
-import StyledButton from "~/components/StyledButton";
+import { Button as BaseButton } from "~/components/ui/button";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,6 +23,15 @@ export const metadata = {
     url: "/favicon",
   },
 };
+
+function StyledButton({ ...props }) {
+  return (
+    <BaseButton
+      className="transform rounded-lg bg-green-500 text-black shadow-md transition-all duration-200 ease-in-out hover:scale-105 hover:bg-green-600 hover:shadow-lg"
+      {...props}
+    ></BaseButton>
+  );
+}
 
 function TopNav() {
   return (
