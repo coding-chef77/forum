ad7ee9aec1a5a52dbd1bfea105313e5665248d0c
Heine Günther <heine_gunther@hotmail.com>
2024-04-24 23:26:36 +0200

    Test if this button works

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,9 +39,9 @@ function TopNav() {
       <div>NullKarbo</div>
       <div>
         <SignedOut>
-          <div className="transform rounded-lg bg-green-500 p-2 text-black shadow-md transition-all duration-200 ease-in-out hover:scale-105 hover:bg-green-600 hover:shadow-lg">
+          <StyledButton>
             <SignInButton>Logg Inn</SignInButton>
-          </div>
+          </StyledButton>
         </SignedOut>
         <SignedIn>
           <div className="flex gap-4">
