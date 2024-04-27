ca3fb70d2cac6655963309cf981b615655ef8544
Heine Günther <heine_gunther@hotmail.com>
2024-04-27 11:03:48 +0200

    Fixed StyledButton

--- src/components/StyledButton.tsx
+++ src/components/StyledButton.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Button as BaseButton } from "~/components/ui/button";
 
 export default function StyledButton({ ...props }) {
@@ -7,6 +5,6 @@ export default function StyledButton({ ...props }) {
     <BaseButton
       className="transform rounded-lg bg-green-500 text-black shadow-md transition-all duration-200 ease-in-out hover:scale-105 hover:bg-green-600 hover:shadow-lg"
       {...props}
-    ></BaseButton>
+    />
   );
 }
